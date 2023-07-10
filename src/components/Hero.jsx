5edd4d78716368d768a223bce78e3e0e2ea1fe2e
markjffrsn <98568870+markjffrsn@users.noreply.github.com>
2023-07-10 19:13:03 +0800

    Initial Commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,13 +5,15 @@ export default function Hero() {
 		<>
 			<main>
 				<section className="container-fluid hero">
-					<h1 className="hero__title">Hello Wordl</h1>
-					<div className="hero__text">
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
-							reprehenderit fugit debitis natus exercitationem? Dolore s oluta
-							labore illo expedita nam?
-						</p>
+					<div className="container">
+						<h1 className="hero__title">Hello World</h1>
+						<div className="hero__text">
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+								reprehenderit fugit debitis natus exercitationem? Dolore s oluta
+								labore illo expedita nam?
+							</p>
+						</div>
 					</div>
 				</section>
 			</main>
