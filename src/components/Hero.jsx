669ad7e27fe60dd77d1ed648f44648c5417ccd9d
markjffrsn <98568870+markjffrsn@users.noreply.github.com>
2023-07-10 19:00:20 +0800

    Inital Commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import '../css/app.scss';
+
+export default function Hero() {
+	return (
+		<>
+			<main>
+				<section className="container-fluid hero">
+					<h1 className="hero__title">This is my Hero!</h1>
+					<div className="hero__text">
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+							reprehenderit fugit debitis natus exercitationem? Dolore soluta
+							labore illo expedita nam?
+						</p>
+					</div>
+				</section>
+			</main>
+		</>
+	);
+}
